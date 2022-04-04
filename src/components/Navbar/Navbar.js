4ea2b6979555b55b34a4f8edaae8210409ff218e
JubayerDev <ajashes858@gmail.com>
2022-04-04 19:29:34 +0600

    navbar added

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Logo from '../../assets/logo.png';
+import './Navbar.css';
 
 const Navbar = () => {
     return (
         <nav>
-            
+            <div className='logo'>
+                <Link to='/'>
+                    <img src={Logo} alt="" />
+                </Link>
+            </div>
+
+            <div className='nav-items'>
+                <Link to='/home'>Home</Link>
+                <Link to='/packages'>Packages</Link>
+                <Link to='/signup'>Sign Up</Link>
+                <Link to='/login'>
+                    <button className='login-btn'>Log In</button>
+                </Link>
+            </div>
         </nav>
     );
 };
